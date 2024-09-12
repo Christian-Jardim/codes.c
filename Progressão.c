78dbecf7f731a81a3b94de67e424aacc0d7ef380
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2024-09-12 19:50:58 -0300

    Progressão.c

--- Progressão.c
+++ Progressão.c
@@ -10,8 +10,10 @@ printf("Digite um número inteiro e positivo: ");
 scanf("%f",&num);
 
 if(num>=1){
+    printf("Os termos são: \n");
     for (dnm=1;dnm<=num;dnm++){
         soma+=(1/dnm);
+        printf("1/%2.f.\n",dnm);
     }
     printf("A soma dos termos é: %f",soma);
 }
