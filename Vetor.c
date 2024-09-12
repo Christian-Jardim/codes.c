6d3c7be13f9262cb1ecc09ca84105454d73a56a9
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2024-09-12 19:55:27 -0300

    Vetor.c

--- Vetor.c
+++ Vetor.c
@@ -1,18 +1,21 @@
+/*Receber os valores de dois vetores e, se forem iguais nas mesmas posições, imprimi-los.*/ 
+
 #include <stdio.h>
 int main(){
 
-int vet[5],x,y;
+int vet1[5],vet2[5],x,y;
 
 for(x=0;x<5;x++){
-    printf("Digite: \n");
-    scanf("%d",&vet[x]);
+    printf("Digite os valores do vetor 1: \n");
+    scanf("%d",&vet1[x]);
 }
 for(x=0;x<5;x++){
-    printf("%d\t\n",vet[x]);
+    printf("Digite os valores do vetor 2:\n");
+    scanf("%d",&vet2[x]);
 }
 for(y=0;y<5;y++){
-    if(vet[y]<0){
-        printf("Dado: %d e posição: %d.\n",vet[y],y+1);
+    if(vet1[y]==vet2[y]){
+        printf("A posição %d possui o mesmo valor: %d\n",y+1,vet1[y]);
     }
 }
 return 0;
