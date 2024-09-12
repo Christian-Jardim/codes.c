ff7241f072f90991d18f76d5c242ab838b6e85c4
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2024-09-12 16:31:18 -0300

    If

---
+++ Progressão.c
@@ -0,0 +1,23 @@
+/*Calcular a soma da progressão: 1+1/2+1/3+...+1/n.*/
+
+#include <stdio.h>
+int main(){
+
+float soma,num,dnm;
+
+printf("Vamos realizar a soma da progressão: 1+1/2+...+1/n.\n");
+printf("Digite um número inteiro e positivo: ");
+scanf("%f",&num);
+
+if(num>=1){
+    for (dnm=1;dnm<=num;dnm++){
+        soma+=(1/dnm);
+    }
+    printf("A soma dos termos é: %f",soma);
+}
+else{
+    printf("Número inválido.\n");
+}
+
+return 0;
+}
