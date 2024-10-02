b9710c013b26f793d63126bd672f58b029c4899c
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2024-10-01 21:26:44 -0300

    Rascunho 2.c

---
+++ Exercícios 09.10.2024/2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+int mat[3][3],i,j;
+printf("Digites os números da matriz: \n");
+for(i=0;i<3;i++){
+ for(j=0;j<3;j++){
+  scanf("%d",&mat[i][j]);
+ }
+}
+for(i=0;i<3;i++){
+ for(j=0;j<3;j++){
+  printf("%i",mat[i][j]);
+ }
+ for(i=0;i<3;i++){
+  
+ }
+ printf("\n");
+}
+return 0;
+}
