0cf8508ba56ed5b1218e252f3d1ce8c66a9fcba1
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2024-10-05 23:47:51 -0300

    1.c

--- Exercícios 24.09.2024/1.c
+++ Exercícios 24.09.2024/1.c
@@ -1,1 +1,18 @@
-#include
+#include <stdio.h>
+
+int main()
+{
+int A[6]={1,0,5,-2,-5,7},sum,i;
+
+printf("A soma dos valores das posições 1, 2 e 6 é: %i\n",
+sum=A[0]+A[1]+A[5]);
+
+A[4]=100;
+
+printf("Os valoresdo vetor são:\n");
+for(i=0;i<6;i++){
+ printf("%i\n",A[i]);
+}
+
+return 0;
+}
