e325855530219f869e6a0cf7f33c73cd1d45e0ca
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2024-11-05 22:00:50 -0300

    Create 1.c

---
+++ Exercício 05.11.2024/1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+
+struct Banda1{
+ char nome[20], tipo[10];
+ int integ, pos;
+}band1;
+
+struct Banda2{
+ char nome[20], tipo[10];
+ int integ, pos;
+}band2;
+
+struct Banda3{
+ char nome[20], tipo[10];
+ int integ, pos;
+}band3;
+
+struct Banda4{
+ char nome[20], tipo[10];
+ int integ, pos;
+}band4;
+
+struct Banda5{
+ char nome[20], tipo[10];
+ int integ, pos;
+}band5;
+
+int main(){
+char b[2];
+b[0] = "b";
+printf("%s",b[0]);
+return 0;
+}
