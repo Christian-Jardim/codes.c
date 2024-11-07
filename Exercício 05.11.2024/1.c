577dde5604112336d38505676418ad1ce94fe47b
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2024-11-07 20:23:48 -0300

    Update 1.c

--- Exercício 05.11.2024/1.c
+++ Exercício 05.11.2024/1.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
-
+int fun(int n){
+ switch (n){
+ case 1:
+  if
+  return 
+ }
+}
 struct Banda1{
  char nome[20], tipo[10];
  int integ, pos;
@@ -28,8 +34,54 @@ struct Banda5{
 }e;
 
 int main(){
+    
+printf("Nome de uma banda: ");
 gets(a.nome);
+printf("Seu gênero musical: ");
 gets(a.tipo);
-scanf()
+printf("O número integrantes dela: ");
+scanf("%i",&a.integ);
+printf("A posição dela no seu ranking: ");
+scanf("%i",&a.rank);
+
+printf("Nome de uma banda: ");
+gets(b.nome);
+printf("Seu gênero musical: ");
+gets(b.tipo);
+printf("O número integrantes dela: ");
+scanf("%i",&b.integ);
+printf("A posição dela no seu ranking: ");
+scanf("%i",&b.rank);
+
+printf("Nome de uma banda: ");
+gets(c.nome);
+printf("Seu gênero musical: ");
+gets(c.tipo);
+printf("O número integrantes dela: ");
+scanf("%i",&c.integ);
+printf("A posição dela no seu ranking: ");
+scanf("%i",&c.rank);
+
+printf("Nome de uma banda: ");
+gets(d.nome);
+printf("Seu gênero musical: ");
+gets(d.tipo);
+printf("O número integrantes dela: ");
+scanf("%i",&d.integ);
+printf("A posição dela no seu ranking: ");
+scanf("%i",&d.rank);
+
+printf("Nome de uma banda: ");
+gets(e.nome);
+printf("Seu gênero musical: ");
+gets(e.tipo);
+printf("O número integrantes dela: ");
+scanf("%i",&e.integ);
+printf("A posição dela no seu ranking: ");
+scanf("%i",&e.rank);
+
+printf("Digite um número entre 1 e 5: ");
+scanf("%i",&n)
+fun(n);
 return 0;
 }
