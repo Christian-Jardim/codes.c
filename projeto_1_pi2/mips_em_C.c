4d7f8373043c722d0967dd582d17c5bf0ca386ca
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-03-17 16:56:41 -0300

    Update mips_em_C.c

--- projeto_1_pi2/mips_em_C.c
+++ projeto_1_pi2/mips_em_C.c
@@ -1,16 +1,62 @@
 #include <stdio.h>
 
-int p_count(pc); 
- 
-int main(){
-  int op,pc; 
+int p_count(pc);
 
-  printf("Menu"
-  switch(op){
-    case 1:
+int main() {
+	int op,pc;
+
+	do {
+		printf("\n *** MENU *** \n");
+		printf("1 - Carregar memoria de instrucoes\n");
+		printf("2 - Carregar memoria de dados\n");
+		printf("3 - Imprimir memorias\n");
+		printf("4 - Imprimir banco de registradores\n");
+		printf("5 - Imprimir todo o simulador\n");
+		printf("6 - Salvar .asm\n");
+		printf("7 - Salvar .dat\n");
+		printf("8 - Executar programa\n");
+		printf("9 - Executar instrucao\n");
+		printf("10 - Volta uma instrucao\n");
+		printf("11 - Sair\n");
+		scanf("%i", &op);
+		
+		do {
+			if (op < 1 || op > 11) {
+				printf("\nOpcao invalida, digite novamente: ");
+				scanf("%i", &op);
+			}
+		} while(op < 1 || op > 11);
+
+		switch (op) {
+		case 1:
+			break;
+		case 2:
+			break;
+  case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			break;
+		case 7:
+			break;
+		case 8:
+			break;
+		case 9:
+			break;
+		case 10:
+			break;
+		case 11:
+		 return 0;
+		 break;
+		}
+	} while(op != 11);
+
 }
 
-int p_count(pc){
-  pc+=1;
-  return pc;
+int p_count(pc) {
+	pc+=1;
+	return pc;
 }
