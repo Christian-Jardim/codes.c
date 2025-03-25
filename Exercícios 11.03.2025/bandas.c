df51b826b144278279482b947938cc28599bc8ef
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-03-25 18:19:48 -0300

    Update bandas.c

--- Exercícios 11.03.2025/bandas.c
+++ Exercícios 11.03.2025/bandas.c
@@ -23,3 +23,28 @@ printf("Lugar no ranking:%d\n",pos.x[i]);
 		}
 	}
 }
+
+void est(char h[10]) {
+	char c[10];
+	printf("Estas são as suas bandas favoritas desse estilo:\n");
+	for(int i=0; i<5; i++) {
+		strcpy(c, gen.x[i]);
+		if(strcmp(c, h) == 0) {
+			printf("%s\n",band.x[i]);
+		}
+	}
+}
+
+void ban(char g[20]) {
+	int y;
+	char c[10];
+	for(int i=0; i<5; i++) {
+		strcpy(c, band.x[i]);
+		if(strcmp(c, g) == 0) {
+			printf("Esta banda é uma das suas favoritas.");
+			y=1;}
+		if(y=0) {
+			printf("Esta banda não está entre as suas favoritas.");
+		}
+	}
+}
