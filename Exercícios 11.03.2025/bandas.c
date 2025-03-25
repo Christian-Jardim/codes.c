816937c96148e9e58cc30ba08093128c38454799
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-03-25 18:09:56 -0300

    Create bandas.c

---
+++ Exercícios 11.03.2025/bandas.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+	char nome[30];
+	char gen[20];
+	int rank;
+	int integ;
+}Band;
+
+
+int main() {
+	Band band[5];
+}
+
+void ran(int r) {
+	for(int i=0; i<5; i++) {
+		if(pos.x[i] == r) {
+			printf("\nBanda:%s\n",band.x[i]);
+			printf("Estilo musical:%s\n",gen.x[i]);
+			printf("Quantidade de integrantes:%d\n",integ.x[i]);
+printf("Lugar no ranking:%d\n",pos.x[i]);
+		}
+	}
+}
