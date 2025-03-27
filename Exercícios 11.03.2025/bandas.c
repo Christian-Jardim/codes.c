aba132a1f42801b9e1f9cdc98af7b9082e1b7e40
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-03-26 23:55:19 -0300

    Update bandas.c

--- Exercícios 11.03.2025/bandas.c
+++ Exercícios 11.03.2025/bandas.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 enum gen{rock=1, samba, metal, sertanejo, jazz};
 
@@ -13,7 +12,7 @@ typedef struct {
 void load(Band band[]);
 void print(Band band[]);
 void ran(Band band[], int r);
-void est(Band band[], gen g);
+void est(Band band[], int g);
 void banVeri(Band band[], char ver[20]);
 
 int main() {
@@ -22,65 +21,126 @@ int main() {
 	Band band[5];
 	load(band);
 	print(band);
+	printf("\nDigite um numero entre 1 e 5: ");
 	scanf("%d",&r);
 	ran(band, r);
+	printf("\nDigite um numero entre 1 e 5: ");
 	scanf("%d", &g);
 	est(band, g);
-	scanf(" %[^\n]", &ver);
-	banVeri(band, ver); 
+	printf("\nDigite o nome de uma banda: ");
+	scanf(" %[^\n]", ver);
+	banVeri(band, ver);
+	return 0;
 }
 
 void load(Band band[]) {
 	for(int i=0; i<5; i++) {
 		printf("\nDigite o nome de uma banda favorita: ");
-		scanf(" %[^\n]", &band[i].nome);
+		scanf(" %[^\n]", band[i].nome);
 		int tipoinf;
-		printf("\nSeu estilo musical, dentro desta opcoes: 1 - rock, 2 - samba, 3 - metal, 4 - sertanejo, 5 - jazz");
+		printf("\nSeu estilo musical, dentro desta opcoes: 1 - rock, 2 - samba, 3 - metal, 4 - sertanejo, 5 - jazz: ");
 		scanf("%d", &tipoinf);
 		band[i].tipo = tipoinf;
-		printf("\nQuantidade de membros: );
+		printf("\nQuantidade de membros: ");
 		scanf("%d",&band[i].integ);
-		printf("\nA posicao dela no seu ranking: ");
+		printf("\nA posicao dela no seu ranking: ");
+
 		scanf("%d",&band[i].rank);
 	}
 }
 
-void print(Band band) {
+void print(Band band[]) {
 	for(int i=0; i<5; i++){
-		printf("\nBanda:%s\n",band[i].nome);
-			printf("Estilo musical: ");
-			switch(band[i].tipo) {
-				case 1:
-					printf("rock");
-			printf("Quantidade de integrantes:%d\n", band[i].integ);
-printf("Lugar no ranking:%d\n",band[i].rank);
+		printf("\nBanda: %s\n",band[i].nome);
+		printf("\nQuantidade de integrantes:%d\n", band[i].integ);
+        printf("\nLugar no ranking:%d\n",band[i].rank);
+		printf("\nEstilo musical: ");
+		switch(band[i].tipo) {
+			case 1:
+				printf("\nrock");
+				break;
+			case 2:
+			   printf("\nsamba");
+				break;
+			case 3:
+			   printf("\nmetal");
+				break;
+			case 4:
+			   printf("\nsertanejo");
+				break;
+			case 5:
+			   printf("\njazz");
+				break;
+        }
+    }
 }
 
 void ran(Band band[], int r) {
 	for(int i=0; i<5; i++) {
 		if(band[i].rank == r) {
-			print(band);
+			printf("\nBanda: %s\n",band[i].nome);
+		printf("\nQuantidade de integrantes:%d\n", band[i].integ);
+        printf("\nLugar no ranking:%d\n",band[i].rank);
+		printf("\nEstilo musical: ");
+		switch(band[i].tipo) {
+			case 1:
+				printf("\nrock");
+				break;
+			case 2:
+			   printf("\nsamba");
+				break;
+			case 3:
+			   printf("\nmetal");
+				break;
+			case 4:
+			   printf("\nsertanejo");
+				break;
+			case 5:
+			   printf("\njazz");
+				break;
 		}
 	}
+}
 }
 
 void est(Band band[], int g) {
 	printf("Estas são as suas bandas favoritas desse estilo:\n");
-	for(int i=0; i<5; i++) {
+	for(int i=0; i<5; i++) {
+
 		if(band[i].tipo == g) {
-			print(band);
+			printf("\nBanda: %s\n",band[i].nome);
+		printf("\nQuantidade de integrantes:%d\n", band[i].integ);
+        printf("\nLugar no ranking:%d\n",band[i].rank);
+		printf("\nEstilo musical: ");
+		switch(band[i].tipo) {
+			case 1:
+				printf("\nrock");
+				break;
+			case 2:
+			   printf("\nsamba");
+				break;
+			case 3:
+			   printf("\nmetal");
+				break;
+			case 4:
+			   printf("\nsertanejo");
+				break;
+			case 5:
+			   printf("\njazz");
+				break;
 		}
 	}
 }
+}
 
-void ban(Band band[], char ver[20]) {
+void banVeri(Band band[], char ver[20]) {
 	int y;
 	for(int i=0; i<5; i++) {
 		if(strcmp(ver, band[i].nome) == 0) {
-			printf("Esta banda é uma das suas favoritas.");
+			printf("\nEsta banda é uma das suas favoritas.\n");
 			y=1;}
 		if(y=0) {
-			printf("Esta banda não está entre as suas favoritas.");
+			printf("\nEsta banda não está entre as suas favoritas.\n");
 		}
 	}
 }
