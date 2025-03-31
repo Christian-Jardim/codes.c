16eaa60b94660cb30b5ec17c71aa2313e5fcbcc4
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-03-31 10:51:24 -0300

    Update armazem_maercado.c

--- Exercícios 11.03.2025/armazem_maercado.c
+++ Exercícios 11.03.2025/armazem_maercado.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
-enum tipo {hortifruti,bebidas,mercearia,higiene,acougue,laticineos,frios,doces,padaria};
+enum tipo {hortifruti=1,bebidas,mercearia,higiene,acougue,laticineos,frios,doces,padaria};
 
-typedef struct produto{
+typedef struct {
 	char nome[15]
 	enum tipo setor;
 	int quant;
@@ -11,21 +11,21 @@ typedef struct produto{
 }ITEM;
 
 void menu();
-void insere();
+void insere(int cont, ITEM *item);
 
 int main() {
 	int cont,op;
-	ITEM item;
 
 	do{
 	printf("Informe a quantidade de produtos a serem cadastrados: ");
 	scanf("%d", &cont);
+	ITEM *item = (ITEM*) calloc(sizeof(ITEM)); 
 	printf("Escolha uma opcao: ");
 	menu();
 	scanf("%d",&op);
 	switch(op) {
 	case 1:
-		insere();
+		insere(cont, item);
 	}
 	}while(op != 10)
 }
@@ -43,6 +43,12 @@ void menu() {
 	printf("10 - Sair");
 }
 
-void insere() {
-
+void insere(int cont, ITEM *item) {
+	int set;
+	printf("Digite o nome do produto: ");
+	scanf("%s", p->nome);
+	printf("1 - Hortifruti\n2 - Bebidas\n3 - Mercearia\n4 - Higiene\n5 - Acougue\n6 - Laticineos\n7 - Frios\n8 - Doces\n9 - Padaria")
+	printf("O setor do produto dentre as opcoes acima: "); 
+	scanf("%d", &set);
+	item->setor = set; 
 }
