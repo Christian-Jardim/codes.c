bdb824ff58f8c88cfca666a8d1df6ac321558ce9
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-03-31 10:05:53 -0300

    Update armazem_maercado.c

--- Exercícios 11.03.2025/armazem_maercado.c
+++ Exercícios 11.03.2025/armazem_maercado.c
@@ -17,6 +17,11 @@ int main() {
 	do{
 	printf("Informe a quantidade de produtos a serem cadastrados: ");
 	scanf("%d", &cont);
-	
+	printf("Escolha uma opcao: ");
+	menu
+	scanf("%d",&op);
+	switch(op) {
+	case 1:
+	}
 	}while(op != 0)
 }
