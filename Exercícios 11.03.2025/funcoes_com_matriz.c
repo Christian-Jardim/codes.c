613b52a99ff47afcdcb40d345da811597c8e76f0
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-03-31 14:00:21 -0300

    Update funcoes_com_matriz.c

--- Exercícios 11.03.2025/funcoes_com_matriz.c
+++ Exercícios 11.03.2025/funcoes_com_matriz.c
@@ -1,141 +1,138 @@
-// Lista 1
-// 16/03/2025
-// Christian de Vargas Jardim (christianjardim.aluno@unipampa.edu.br)
-
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
-int **criaMatriz(int m, int n);
-void leiaMatriz(int **mat, int m, int n);
-void imprimeMatriz(int **mat, int m, int n);
-int somaMatriz(int **mat, int m, int n);
-int *colunaMat(int **mat, int m, int ncoluna);
-void imprimeVet(int *vet, int n);
-void liberaVet(int *vet);
-void liberaMatriz(int **mat, int m);
+enum tipo {hortifruti=1,bebidas,mercearia,higiene,acougue,laticineos,frios,doces,padaria};
+
+typedef struct {
+	char nome[15];
+	enum tipo setor;
+	int quant;
+	float valor;
+} ITEM;
+
+void menu();
+int insere(int cont, ITEM *item);
+void apresenta(char prod, ITEM *item);
+
+
+void buscaN(char bnome[15], ITEM *item, int qt);
+void buscaS(int snome, ITEM *item, int qt);
 
 int main() {
-	int op,m,n,**mat=NULL,sum,*vet,ncoluna;
+	int cont,op,qt,snome;
+	char bnome[15];
 
 	do {
-		printf("\nO que desejas fazer?\n");
-		printf("1 - Criar uma matriz\n");
-		printf("2 - Ler os elementos de uma matriz\n");
-		printf("3 - Imprimir a matriz\n");
-		printf("4 - Receber a soma dos elementos da matriz\n");
-		printf("5 - Retornar os elementos de uma determinada coluna da matriz\n");
-		printf("6 - Sair do programa\n\n");
-		scanf("%i", &op);
-		
-		do {
-			if (op < 1 || op > 6) {
-				printf("\nNumero invalido, digite novamente: ");
-				scanf("%i", &op);
-			}
-		} while(op < 1 || op > 6);
-
-		switch (op) {
+		printf("Informe a quantidade de produtos a serem cadastrados: ");
+		scanf("%d", &cont);
+		ITEM *item = (ITEM *) calloc(cont, sizeof(ITEM));
+		menu();
+		printf("Escolha uma opcao dentre as acima: ");
+		scanf("%d",&op);
+		switch(op) {
 		case 1:
-			printf("\nDigite a quantidade de linhas: ");
-			scanf("%i", &m);
-			printf("Digite a quantidade de colunas: ");
-			scanf("%i", &n);
-			printf("\n");
-			mat = criaMatriz(m, n);
+			qt = insere(cont, item);
 			break;
 		case 2:
-			leiaMatriz(mat, m, n);
+			printf("Informe o produto que deseja buscar: ");
+			scanf(" %[^\n]", bnome);
+			buscaN(bnome, item, qt);
 			break;
 		case 3:
-			imprimeMatriz(mat, m, n);
+
 			break;
 		case 4:
-			sum = somaMatriz(mat, m, n);
-			printf("\nA soma dos elementos da matriz e: %i\n", sum);
+
 			break;
 		case 5:
-			printf("\nDigite qual coluna voce quer: ");
-			scanf("%i", &ncoluna);
-			vet = colunaMat(mat, m, ncoluna);
-			imprimeVet(vet, m);
+			printf("Informe o produto que deseja buscar: ");
+			scanf(" %[^\n]", bnome);
+			buscaN(bnome, item, qt);
 			break;
 		case 6:
-			liberaVet(vet);
-			liberaMatriz(mat, m);
-			return 0;
+			printf("1 - Hortifruti\n2 - Bebidas\n3 - Mercearia\n4 - Higiene\n5 - Acougue\n6 - Laticineos\n7 - Frios\n8 - Doces\n9 - Padaria\n\n");
+			printf("Infoeme o setor do produto dentre as opcoes acima: ");
+			scanf("%d", &snome);
+			buscaS(snome, item, qt);
 			break;
+		case 7:
+			printf("Informe o produto que foi vendido: ");
+			scanf(" %[^\n]", v);
+			qt = venda(v, item, qt);
+			break;
+		case 8:
+
+			break;
+		case 9:
+
+			break;
+		case 10:
+			free(item);
+			return 0;
 		}
-	} while(op != 6);
+	} while(op != 10);
 }
 
-int **criaMatriz(int m, int n) {
-	int **mat,i,j;
-	mat = malloc(sizeof (int*)*m);
-	for (i=0; i < m; i++) {
-		mat[i] = malloc(sizeof (int)*n);
+void menu() {
+	printf("\n1 - Inserir produtos\n");
+	printf("2 - Apresentar um produto\n");
+	printf("3 - Apresentar todos os produtos");
+	printf("4 - Balanco\n");
+	printf("5 - Busca por nome\n");
+	printf("6 - Busca por setor\n");
+	printf("7 - Venda\n");
+	printf("8 - Carregar arquivo\n");
+	printf("9 - Gerar arquivo\n");
+	printf("10 - Sair\n\n");
+}
+
+int insere(int cont, ITEM *item) {
+	int set,qt;
+
+	for(int i=0; i<cont; i++) {
+		printf("\nDigite o nome do produto: ");
+		scanf(" %[^\n]", item[i].nome);
+		printf("1 - Hortifruti\n2 - Bebidas\n3 - Mercearia\n4 - Higiene\n5 - Acougue\n6 - Laticineos\n7 - Frios\n8 - Doces\n9 - Padaria\n\n");
+		printf("O setor do produto dentre as opcoes acima: ");
+		scanf("%d", &set);
+		item[i].setor = set;
+		printf("Digite a quantidade do produto: ");
+		scanf("%d", &item[i].quant);
+		printf("Digite o valor do produto: ");
+		scanf("%f", &item[i].valor);
+		qt++;
+		return qt;
 	}
-	return mat;
 }
 
-void leiaMatriz(int **mat, int m, int n) {
-	int i, j;
-	for(i=0; i<m; i++) {
-		for(j=0; j<n; j++) {
-			printf("\nDigite a posicao %i%i da matriz: ", i+ 1,j+ 1);
-			scanf("%i", &mat[i][j]);
+void buscaN(char bnome[15], ITEM *item, int qt) {
+	for(int i=0; i<qt; i++) {
+		if(strcmp(bnome, item[i].nome) == 0) {
+			printf("%s\n", item[i].nome);
+			printf("%d\n", item[i].setor);
+			printf("%d\n", item[i].quant);
+			printf("%2.f\n", item[i].valor);
 		}
 	}
 }
 
-void imprimeMatriz(int **mat, int m, int n) {
-	int i, j;
-	printf("\n");
-	printf("\n");
-	for(i=0; i<m; i++) {
-		for(j=0; j<n; j++) {
-			printf("%i  ",mat[i][j]);
-		}
-		printf("\n");
-	}
-}
-
-int somaMatriz(int **mat, int m, int n) {
-	int i, j, sum=0;
-
-	for(i=0; i<m; i++) {
-		for(j=0; j<n; j++) {
-			sum+=mat[i][j];
+void buscaS(int snome, ITEM *item, int qt) {
+	for(int i=0; i<qt; i++) {
+		if(snome == item[i].setor) {
+			printf("%s\n", item[i].nome);
+			printf("%d\n", item[i].setor);
+			printf("%d\n", item[i].quant);
+			printf("%2.f\n", item[i].valor);
 		}
 	}
-	return sum;
 }
 
-int *colunaMat(int **mat, int m, int ncoluna) {
-	int *vet,i;
-	vet = malloc(sizeof(int*)*m);
-	for(i=0; i<m; i++) {
-		vet[i] = mat[i][ncoluna - 1];
+int venda(char v[15], ITEM *item, int qt) {
+	for(int i=0; i<qt; i++) {
+		if(strcmp(v, item[i].nome) == 0) {
+			qt--;
+		}
 	}
-	return vet;
-}
-
-void imprimeVet(int *vet, int m) {
-	int i;
-	printf("\n");
-	for(i=0; i<m; i++) {
-		printf("%i  ", vet[i]);
-	}
-	printf("\n");
-}
-
-void liberaVet(int *vet) {
-	free(vet);
-}
-
-void liberaMatriz(int **mat, int m) {
-	int i;
-	for(i=0; i<m; i++) {
-		free (mat[i]);
-	}
-	free(mat);
+	return qt;
 }
