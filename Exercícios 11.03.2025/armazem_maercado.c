904032581e9d98221d7f88fbf05dcf7cd8639393
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-03-31 09:11:04 -0300

    Update armazem_maercado.c

--- Exercícios 11.03.2025/armazem_maercado.c
+++ Exercícios 11.03.2025/armazem_maercado.c
@@ -11,6 +11,12 @@ typedef struct {
 }ITEM;
 
 int main() {
+	int cont,op;
+	ITEM item
 
-
+	do{
+	printf("Informe a quantidade de produtos a serem cadastrados: ");
+	scanf("%d", &cont);
+	
+	}while(op != 0)
 }
