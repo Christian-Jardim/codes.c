c52f18b236bbd651cb5bfcdb71ca8c98d43aade0
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-03-31 10:27:59 -0300

    Update armazem_maercado.c

--- Exercícios 11.03.2025/armazem_maercado.c
+++ Exercícios 11.03.2025/armazem_maercado.c
@@ -10,18 +10,39 @@ typedef struct produto{
 	float valor;
 }ITEM;
 
+void menu();
+void insere();
+
 int main() {
 	int cont,op;
-	ITEM item
+	ITEM item;
 
 	do{
 	printf("Informe a quantidade de produtos a serem cadastrados: ");
 	scanf("%d", &cont);
 	printf("Escolha uma opcao: ");
-	menu
+	menu();
 	scanf("%d",&op);
 	switch(op) {
 	case 1:
+		insere();
 	}
-	}while(op != 0)
+	}while(op != 10)
+}
+
+void menu() {
+	printf("1 - Inserir produtos");
+	printf("2 - Apresentar um produto");
+	printf("3 - Apresentar todos os produtos");
+	printf("4 - Balanco");
+	printf("5 - Busca por nome");
+	printf("6 - Busca por setor");
+	printf("7 - Venda");
+	printf("8 - Carregar arquivo");
+	printf("9 - Gerar arquivo");
+	printf("10 - Sair");
+}
+
+void insere() {
+
 }
