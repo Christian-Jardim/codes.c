fb98f300848e0078645398fae34eb295de6fd410
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-04-02 12:41:31 -0300

    Update armazem_mercado.c

--- Exercícios 11.03.2025/armazem_maercado.c
+++ Exercícios 11.03.2025/armazem_maercado.c
@@ -9,83 +9,66 @@ typedef struct {
 	enum tipo setor;
 	int quant;
 	float valor;
-}ITEM;
+}Item;
 
 void menu();
-void insere(int cont, ITEM *item);
+Item* realocar(Item *item);
+int insere(Item *item, int qt);
+
 
 int main() {
-	int cont,op;
-
-	do{
-	printf("Informe a quantidade de produtos a serem cadastrados: ");
-	scanf("%d", &cont);
-	ITEM *item = (ITEM*) malloc(cont*sizeof(ITEM)); 
-	menu();
-	printf("Escolha uma opcao dente as acima: ");
-	scanf("%d",&op);
-	switch(op) {
-	case 1:
-		insere(cont, item);
-		break;
-	case 2:
-		
-		break;
-	case 3:
-		insere(cont, item);
-		break;
-	case 4:
-		insere(cont, item);
-		break;
-	case 5:
-		insere(cont, item);
-		break;
-	case 6:
-		insere(cont, item);
-		break;
-	case 7:
-		insere(cont, item);
-		break;
-	case 8:
-		insere(cont, item);
-		break;
-	case 9:
-		insere(cont, item);
-		break;
-	case 10:
-		insere(cont, item);
-		break;
-	}
-	}while(op != 10);
+    int cont,op,qt=0;
+    
+    printf("Insira quantos produtos serao cadastrados: ");
+    scanf("%d", &cont);
+    Item *item = (Item*)malloc(sizeof(Item) * cont);
+    
+    do{
+        menu();
+        printf("\nEscolha uma das opcoes acima: ");
+        scanf("%d", &op);
+        switch(op) {
+            case 1:
+                item = realocar(item);
+                break;
+            case 2:
+                qt = insere(item,qt);
+        }
+    } while(op != 11);
+    printf("%s",item[qt- 1].nome);
 }
 
 void menu() {
-	printf("\n1 - Inserir produtos\n");
-	printf("2 - Apresentar um produto\n");
-	printf("3 - Apresentar todos os produtos");
-	printf("4 - Balanco\n");
-	printf("5 - Busca por nome\n");
-	printf("6 - Busca por setor\n");
-	printf("7 - Venda\n");
-	printf("8 - Carregar arquivo\n");
-	printf("9 - Gerar arquivo\n");
-	printf("10 - Sair\n\n");
+    printf("\n1 - Escolher quantos produtos a mais serao cadastrados");
+    printf("\n2 - Inserir um produto");
+    printf("\n3 - Escolher quantos elementos serao cadastrados");
+    printf("\n3 - Escolher quantos elementos serao cadastrados");
+    printf("\n4 - Escolher quantos elementos serao cadastrados");
+    printf("\n5 - Escolher quantos elementos serao cadastrados");
+    printf("\n6 - Escolher quantos elementos serao cadastrados");
+    printf("\n7 - Escolher quantos elementos serao cadastrados");
+    printf("\n8 - Escolher quantos elementos serao cadastrados");
+    printf("\n9 - Escolher quantos elementos serao cadastrados");
+    printf("\n10 - Escolher quantos elementos serao cadastrados");
+    printf("\n11 - Sair");
 }
 
-void insere(int cont, ITEM *item) {
-	int set,qt;
-	
-	for(int i=0; i<cont; i++){
-	    printf("Digite o nome do produto: ");
-	    scanf("%s", item->nome);
-	    printf("1 - Hortifruti\n2 - Bebidas\n3 - Mercearia\n4 - Higiene\n5 - Acougue\n6 - Laticineos\n7 - Frios\n8 - Doces\n9 - Padaria\n\n");
-	    printf("O setor do produto dentre as opcoes acima: "); 
-	    scanf("%d", &set);
-	    item->setor = set;
-	    printf("Digite a quantidade do produto: ");
-	    scanf("%d", &item->quant);
-	    printf("Digite o valor do produto: ");
-	    scanf("%f", &item->valor);
-	    qt++;
-	}
+Item *realocar(Item *item) {
+    int mp;
+    
+    printf("\nInsira quantos elementos a mais serao cadastrados: ");
+    scanf("%d", &mp);
+    
+    item = (Item*)realloc(item,sizeof(Item*) * mp);
+    
+    return item;
+}
+
+int insere(Item *item, int qt) {
+    
+    printf("Digite o nome do produto: ");
+    scanf(" %[^\n]", item[qt].nome);
+    
+    qt++;
+    return qt;
 }
