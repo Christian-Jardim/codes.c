9f22d90937576f103594d04bdb22d8a9e1303d12
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-04-04 16:14:25 -0300

    Update armazem__interface_christian.h

--- Exercícios 11.03.2025/armazem__interface_christian.h
+++ Exercícios 11.03.2025/armazem__interface_christian.h
@@ -1,1 +1,18 @@
+enum tipo {hortifruti=1,bebidas,mercearia,higiene,acougue,laticineos,frios,doces,padaria};
 
+typedef struct {
+	char nome[15];
+	enum tipo setor;
+	int quant;
+	float valor;
+} Item;
+
+void menu();
+Item* realocar(Item *item);
+int insere(Item *item, int qt);
+void apresenta(Item *item, int qt);
+void apresenta1(Item *item, int qt);
+void apresentaT(Item *item, int qt);
+void buscaN(Item *item, int qt);
+void buscaS(Item *item, int qt);
+void venda(Item *item, int qt);
