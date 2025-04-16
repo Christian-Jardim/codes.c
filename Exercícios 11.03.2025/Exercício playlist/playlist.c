a9c373336693d0180de1cd4cbba1d2c5630aaef8
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-04-16 16:55:02 -0300

    Update playlist.c

--- Exercícios 11.03.2025/Exercício playlist/playlist.c
+++ Exercícios 11.03.2025/Exercício playlist/playlist.c
@@ -1,1 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "playcj.h"
+
+int main() {
+    int op;
+	do {
+		menu();
+		printf("\nEscolha uma das opcoes acima: ");
+		scanf("%d", &op);
+		switch(op) {
+		case 1:
+            desc *playlist=cria_desc();
+			break;
+		case 2:
+		    nodo *node=cria_nodo();
+		    musica *song=cria_espaco();
+		    insere(playlist,node,song);
+			break;
+		case 3:
+
+			break;
+		case 4:
+
+			break;
+		case 5:
+
+			break;
+		case 6:
+			printf("\nVoce saiu!");
+			break;;
+		default:
+			printf("\nOpcao invalida");
+		}
+	} while(op != 6);
+	return 0;
+}
