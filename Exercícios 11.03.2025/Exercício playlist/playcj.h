74248b7f8887bdba4e160f3675939d8b6a676b84
ChristianJardim1 <christianjardim.aluno@unipampa.edu.br>
2025-04-16 16:56:11 -0300

    Create playcj.h

---
+++ Exercícios 11.03.2025/Exercício playlist/playcj.h
@@ -0,0 +1,27 @@
+typedef struct musica musica;
+typedef struct nodo nodo;
+typedef struct desc desc;
+
+struct musica {
+	char titulo[256];
+	char artista[256];
+	char letra[256];
+	int codigo;
+	int execucoes;
+};
+
+struct nodo {
+	nodo *prox;
+	musica *info;
+};
+
+struct desc {
+	nodo *primeiro_nodo;
+	int tamanho;
+};
+
+void menu();
+desc *cria_desc();
+nodo *cria_nodo();
+musica *cria_espaco();
+void insere(desc *p, nodo *n, musica *s);
