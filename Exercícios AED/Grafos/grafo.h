e4c4dc7bff598046a8d92eb05e2902a8967bb763
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-07-08 21:28:11 -0300

    Update grafo.h

--- Exercícios AED/Grafos/grafo.h
+++ Exercícios AED/Grafos/grafo.h
@@ -49,3 +49,25 @@ struct descritor_grafo_matriz* parserMatriz(char *nomeArquivo);
 struct descritor_grafo_matriz * inicializaGrafoMatriz(int tamanho);
 struct descritor_grafo_matriz * insereArestaMatriz(struct descritor_grafo_matriz *grafo, int chaveSaida, int chaveChegada, int peso);
 void imprimeGrafoMatriz(struct descritor_grafo_matriz *grafoMatriz);
+
+//////-----------------STACK.C
+
+struct nodopilha{
+	struct aresta *arestaPilha;
+	struct nodopilha *prox;
+};
+
+struct desc_stack{
+	struct nodopilha *top;
+	int tamanho;
+};
+
+struct desc_stack *criaDescStack(void);
+struct nodopilha* criaNodoStack(struct aresta *arestaPilha);
+void push(struct desc_stack *stack,struct nodopilha*novoElemento);
+struct nodopilha* pop(struct desc_stack *stack);
+int empty(struct desc_stack *stack);
+int length(struct desc_stack *stack);
+void makeNull(struct desc_stack *stack);
+struct nodopilha* top(struct desc_stack *stack);
+void showStack(struct desc_stack *stack);
