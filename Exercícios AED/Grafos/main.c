d0f2a0c0b6748775d8c8ef25cef5fd44413bd9e3
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-07-14 16:00:57 -0300

    Update main.c

--- Exercícios AED/Grafos/main.c
+++ Exercícios AED/Grafos/main.c
@@ -12,7 +12,7 @@ int main() {
 	struct desc_stack *minhaPilha = NULL;
 	opcao = -1;
 	while(opcao != 0) {
-		printf(" Escolha sua opção:\n");
+		printf("\n Escolha sua opção:\n");
 		printf(" 1 - Carrega grafo lista\n 2 - Busca adjacências\n 3 - Imprime grafo lista\n 4 - Pilha de arestas\n 5 - Carrega grafo Matriz\n 6 - Imprime grafo matriz\n 7 - Busca BFS\n 0 - SAIR\n");
 		setbuf(stdin,NULL);
 		scanf("%d",&opcao);
@@ -20,30 +20,30 @@ int main() {
 		case 1:
 			grafo = parser();
 			if(grafo!=NULL)
-				printf(" Grafo lista inicializado com sucesso\n");
+				printf("\n Grafo lista inicializado com sucesso");
 			break;
 		case 2:
-			printf(" Digite a chave a ser buscada no grafo lista: \n");
+			printf("\n Digite a chave a ser buscada no grafo lista: ");
 			setbuf(stdin,NULL);
 			scanf("%d",&chave_busca);
 			struct nodo* temp = buscaVertice(grafo,chave_busca);
 			if(temp == NULL)
-				printf(" Não encontrou o nodo\n");
+				printf("\n Não encontrou o nodo");
 			else
-				printf(" Encontrou o nodo\n");
+				printf("\n Encontrou o nodo");
 			break;
 		case 3:
 			imprimeGrafo(grafo);
 			break;
 		case 4:
 			if(grafo ==NULL) {
-				printf("grafo lista inicializado com sucesso\n");
+				printf("\n Grafo lista inicializado com sucesso");
 				break;
 			}
 			minhaPilha = criaDescStack();
 
 			//grafo pronto vai acessar as arestas para criar no da pilha e inserir
-			struct aresta *arestaGrafo=NULL;
+			struct aresta *arestaGrafo = NULL;
 			//recebe o primeiro da lista de vertices
 			struct nodo *auxVertice = grafo->nodos;
 
@@ -53,7 +53,7 @@ int main() {
 				arestaGrafo = auxVertice->adjacencias;
 				while(arestaGrafo != NULL) {
 					//crio nC3 pilha para inserir na pilha
-					struct nodopilha *noPilha = criaNodoStack(arestaGrafo);
+					struct nodo_busca *noPilha = criaNodoStack(arestaGrafo);
 					//insere na pilha
 					push(minhaPilha,noPilha);
 					arestaGrafo = arestaGrafo->prox;
@@ -74,9 +74,9 @@ int main() {
 			imprimeGrafoMatriz(grafoMatriz);
 			break;
 		case 7:
-		printf("Chave para começar a busca: ");
+		printf("\n Chave para começar a busca: ");
 		scanf("%d",&chave_busca);
-			bfs(grafo, chave_busca);
+			
 			break;
 		case 0:
 			exit(-1);
