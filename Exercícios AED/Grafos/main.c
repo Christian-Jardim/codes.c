4c13fa62e78d914163d48b825dc0fc5c08c55ca6
Christian Jardim <christianjardim.aluno@unipampa.edu.br>
2025-07-14 13:14:11 -0300

    Update main.c

--- Exercícios AED/Grafos/main.c
+++ Exercícios AED/Grafos/main.c
@@ -1,79 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "stack.h"
+#include <locale.h>
+#include "grafo.h"
 
-int main(){
-	
+int main() {
+    
+    setlocale(LC_ALL,"");
 	int opcao,chave_busca;
 	struct descritor_grafo *grafo = NULL;
 	struct descritor_grafo_matriz *grafoMatriz = NULL;
 	struct desc_stack *minhaPilha = NULL;
 	opcao = -1;
-	while(opcao != 0){
-		printf("escolha sua opcao:\n");
-		printf("1-carrega grafo lista\n2-busca adjacencias\n3-imprime grafo lista\n4-pilhaArestas\n5-carrega grafo Matriz\n6-imprime grafo matriz\n0-SAIR\n");
+	while(opcao != 0) {
+		printf(" Escolha sua opção:\n");
+		printf(" 1 - Carrega grafo lista\n 2 - Busca adjacências\n 3 - Imprime grafo lista\n 4 - Pilha de arestas\n 5 - Carrega grafo Matriz\n 6 - Imprime grafo matriz\n 0 - SAIR\n");
 		setbuf(stdin,NULL);
 		scanf("%d",&opcao);
-		switch(opcao){
-			case 1:
-					grafo = parser("grafo.txt");
-					if(grafo !=NULL)
-						printf("grafo lista inicializado com sucesso\n");
-					break;
-			case 2:
-					printf("digite a chave a ser buscada no grafo lista\n");
-					setbuf(stdin,NULL);
-					scanf("%d",&chave_busca);
-					struct nodo* temp = buscaVertice(grafo,chave_busca);
-					if(temp == NULL)
-						printf("não encontrou o nodo\n");
-					else
-						printf("encontrou o nodo\n");
-					break;
-			case 3:
-					imprimeGrafo(grafo);
-					break;
-			case 4:
-					if(grafo ==NULL){
-						printf("grafo lista inicializado com sucesso\n");
-						break;
-					}
-					minhaPilha = criaDescStack();
+		switch(opcao) {
+		case 1:
+			grafo = parser();
+			if(grafo!=NULL)
+				printf(" Grafo lista inicializado com sucesso\n");
+			break;
+		case 2:
+			printf(" Digite a chave a ser buscada no grafo lista: \n");
+			setbuf(stdin,NULL);
+			scanf("%d",&chave_busca);
+			struct nodo* temp = buscaVertice(grafo,chave_busca);
+			if(temp == NULL)
+				printf(" Não encontrou o nodo\n");
+			else
+				printf(" Encontrou o nodo\n");
+			break;
+		case 3:
+			imprimeGrafo(grafo);
+			break;
+		case 4:
+			if(grafo ==NULL) {
+				printf("grafo lista inicializado com sucesso\n");
+				break;
+			}
+			minhaPilha = criaDescStack();
 
-					//grafo pronto vai acessar as arestas para criar no da pilha e inserir
-					struct aresta *arestaGrafo=NULL;
-					//recebe o primeiro da lista de vertices
-					struct nodo *auxVertice = grafo->nodos;
-					
-					while(auxVertice != NULL){
-					
-						//percorre as adjacencias do auxvertice colocando na pilha
-						arestaGrafo = auxVertice->adjacencias;
-						while(arestaGrafo != NULL){
-							//crio nó pilha para inserir na pilha
-							struct nodopilha *noPilha = criaNodoStack(arestaGrafo);
-							//insere na pilha
-							push(minhaPilha,noPilha);
-							arestaGrafo = arestaGrafo->prox;
-						}
-						auxVertice = auxVertice->prox;
-					}
-					
-					//mostra pilha completa
-					showStack(minhaPilha);
-					
-					break;
-			case 5:
-					grafoMatriz = parserMatriz("grafo.txt");
-					if(grafoMatriz !=NULL)
-						printf("grafo inicializado com sucesso\n");
-					break;
-			case 6:
-					imprimeGrafoMatriz(grafoMatriz);
-					break;
-			case 0:
-					exit(-1);
-		}	
+			//grafo pronto vai acessar as arestas para criar no da pilha e inserir
+			struct aresta *arestaGrafo=NULL;
+			//recebe o primeiro da lista de vertices
+			struct nodo *auxVertice = grafo->nodos;
+
+			while(auxVertice != NULL) {
+
+				//percorre as adjacencias do auxvertice colocando na pilha
+				arestaGrafo = auxVertice->adjacencias;
+				while(arestaGrafo != NULL) {
+					//crio nC3 pilha para inserir na pilha
+					struct nodopilha *noPilha = criaNodoStack(arestaGrafo);
+					//insere na pilha
+					push(minhaPilha,noPilha);
+					arestaGrafo = arestaGrafo->prox;
+				}
+				auxVertice = auxVertice->prox;
+			}
+
+			//mostra pilha completa
+			showStack(minhaPilha);
+
+			break;
+		case 5:
+			grafoMatriz = parserMatriz("grafo.txt");
+			if(grafoMatriz !=NULL)
+				printf("grafo inicializado com sucesso\n");
+			break;
+		case 6:
+			imprimeGrafoMatriz(grafoMatriz);
+			break;
+		case 0:
+			exit(-1);
+		}
 	}
 	return 0;
 }
